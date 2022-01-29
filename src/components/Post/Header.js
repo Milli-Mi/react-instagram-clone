1e0208139fed2ce016e3f491f89768d0b7d5c21a
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-01-29 18:40:19 +0300

    Initial commit

--- src/components/Post/Header.js
+++ src/components/Post/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Header({username, profileSrc}) {
      
-    return  (
-    <a href ="#">
-        <img src = {profileSrc} alt = {username} />
-    <p>{username }</p>
-     </a>)
+    return (
+        <a href='#'>
+            <img src={profileSrc} alt={username}   />
+            <p>{username}</p>
+        </a>
+    );
 }
