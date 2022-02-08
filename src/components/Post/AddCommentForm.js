b65624d47598351e9458dd0d2b9bab167e2b713c
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-02-08 03:19:01 +0300

    adding a function to send a comment
    Update AddCommentForm.js

--- src/components/Post/AddCommentForm.js
+++ src/components/Post/AddCommentForm.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
 export default class AddCommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comment: '',
+        };
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+handleCommentChange = (e) => {
+  this.setState({
+    comment: e.target.value
+  })
+}
+
     render() {
         const { commentInput } = this.props;
+        const { comment} = this.state;
 
         return (
-            <div className='flex border-t'>
+            <form className='flex border-t' onSubmit={this.handleSubmit}>
                 <input
                     type='text'
                     name='comment'
                     placeholder='Add a comment...'
                     className='w-full mr-4 py-2 px-4 text-gray-400 text-sm'
-                    ref = { commentInput }
+                    ref={commentInput}
+                    onChange = {this.handleCommentChange}
+                    value = {comment}
                 />
 
                 <button
@@ -20,7 +40,7 @@ export default class AddCommentForm extends Component {
                 >
                     Submit
                 </button>
-            </div>
+            </form>
         );
     }
 }
