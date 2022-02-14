800e67709a8346cd12c3c5219391fdfabb808922
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-02-15 01:11:31 +0300

    adding a comment by the user

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,77 @@
- import React, { Component } from 'react';
- import Post from './Post'
- import images from '../images/kot-cobi.jpg'
- import profileImg from '../images/profile.jpg'
- import './app.css'
- 
- 
- export default class App extends Component {
-   render() {
-     const post = {
-         src: images,
-         caption: 'cat smuxzi',
-         username: 'Milli',
-         profileSrc: profileImg,
-         likes: 387,
-         comments: [
-             {
-                 text: 'cute cats with blue eyes',
-                 username: 'happy - dog',
-             },
-             {
-                 text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
-                 username: 'Dark_you',
-             },
-             {
-                 text: 'He’s definitely one handsome fella.',
-                 username: 'Jecky',
-             },
-             {
-                 text: 'cute cats with blue eyes',
-                 username: 'happy - dog',
-             },
-             {
-                 text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
-                 username: 'Dark_you',
-             },
-             {
-                 text: 'He’s definitely one handsome fella.',
-                 username: 'Jecky',
-             },
-             {
-                 text: 'cute cats with blue eyes',
-                 username: 'happy - dog',
-             },
-             {
-                 text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
-                 username: 'Dark_you',
-             },
-             {
-                 text: 'He’s definitely one handsome fella.',
-                 username: 'Jecky',
-             },
-         ],
-     }; 
-     return (
-         <div className="grid grid-cols-12">
-             <Post post={post} />
-         </div>
-     ); 
-   } 
- }
+import React, { Component } from 'react';
+import Post from './Post';
+import images from '../images/kot-cobi.jpg';
+import profileImg from '../images/profile.jpg';
+import './app.css';
 
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: {
+                src: images,
+                caption: 'cat smuxzi',
+                username: 'Milli',
+                profileSrc: profileImg,
+                likes: 387,
+                comments: [
+                    {
+                        text: 'cute cats with blue eyes',
+                        username: 'happy - dog',
+                    },
+                    {
+                        text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
+                        username: 'Dark_you',
+                    },
+                    {
+                        text: 'He’s definitely one handsome fella.',
+                        username: 'Jecky',
+                    },
+                    {
+                        text: 'cute cats with blue eyes',
+                        username: 'happy - dog',
+                    },
+                    {
+                        text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
+                        username: 'Dark_you',
+                    },
+                    {
+                        text: 'He’s definitely one handsome fella.',
+                        username: 'Jecky',
+                    },
+                    {
+                        text: 'cute cats with blue eyes',
+                        username: 'happy - dog',
+                    },
+                    {
+                        text: 'his eyes are perfectly contoured, almost like he’s wearing eyeliner',
+                        username: 'Dark_you',
+                    },
+                    {
+                        text: 'He’s definitely one handsome fella.',
+                        username: 'Jecky',
+                    },
+                ],
+            },
+        };
+    }
 
- 
- 
+    handleAddComment = (text) => {
+        const { post } = this.state;
+        this.setState({
+            post: {
+                ...post,
+                comments: [...post.comments, {text, username: 'Milli'}]
+            },
+        });
+    };
+
+    render() {
+         const { post } = this.state;
+        return (
+            <div className='grid grid-cols-12'>
+                <Post post={post} />
+            </div>
+        );
+    }
+}
